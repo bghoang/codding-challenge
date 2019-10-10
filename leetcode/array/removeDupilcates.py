0dc31cb68fb393333bf973045464c7f0d80b2c72
Bao <bghoang.ucsd.edu>
2019-10-10 15:48:55 -0700

    add removeDuplicate

--- leetcode/array/removeDupilcates.py
+++ leetcode/array/removeDupilcates.py
@@ -2,5 +2,21 @@
 Given a sorted array nums, remove the duplicates in-place such 
 that each element appear only once and return the new length.
 """
+def removeDuplicates(self, nums):
+    l = len(nums)
+    # Edge case when l only has 1 element
+    if l < 2:
+        return l
 
+    slowStart = 0
+    # Start loop with 1 to last index
+    for i in range(1,l):
+        # Checking backward
+        # Fix value at 1 index then check the value from the next index to last
+        # If not equal then move to the next index (slow_index+1)
+        if nums[i] != nums[slowStart]:
+            slowStart += 1
+            nums[slowStart] = nums[i]
+
+    return slowStart + 1
 
