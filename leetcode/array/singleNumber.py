af3de3dde3e536d6d0711ed8fe8b9e23e1d0928c
Bao <bghoang.ucsd.edu>
2019-10-21 12:02:21 -0700

    2 more problems

---
+++ leetcode/array/singleNumber.py
@@ -0,0 +1,24 @@
+'''
+Given a non-empty array of integers, every element appears twice 
+except for one. Find that single one.
+
+Input: [2,2,1]
+Output: 1
+'''
+
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:  
+        d = dict()        
+        # Generate key value pair from nums
+        for e in nums:
+            if (e not in d):
+                d[e] = 1
+            else:
+                d[e] += 1
+        
+        # Check in d, return key with value of 1
+        for key,value in d.items():
+            if value == 1:
+                return key
