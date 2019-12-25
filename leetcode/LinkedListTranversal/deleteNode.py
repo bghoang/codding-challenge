778695f09c7b084a8312862658d9058427b35127
Bao <bghoang.ucsd.edu>
2019-12-24 23:15:00 -0800

    add deleteNode

--- leetcode/LinkedListTranversal/deleteNode.py
+++ leetcode/LinkedListTranversal/deleteNode.py
@@ -9,9 +9,12 @@ given only access to that node.
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def deleteNode(self, node):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        node.val = node.next.val
+        node.next = node.next.next
