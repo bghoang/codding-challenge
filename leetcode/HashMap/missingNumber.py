886a295f08c0ca105ae6eb6efa5c038770842c29
Bao <bghoang.ucsd.edu>
2019-12-25 13:40:06 -0800

    add missingnumber

---
+++ leetcode/HashMap/missingNumber.py
@@ -0,0 +1,36 @@
+'''
+Brute force, sort the array
+create another list go from 0 to n
+compare each element in the original list with this list
+return if there is a missmatch
+O(nlogn) time/ O(logn) space
+
+Better solution, using hashmap 
+Loop through the list, populate the hashmap,
+Loop through 0->n again checking if there any missing number
+return that number
+
+Optimal: bit manipulation
+Honestly dont know how it works
+'''
+
+
+class Solution:
+    def missingNumber(self, nums):
+        # My solution
+        d = dict()
+        for num in nums:
+            d[num] = 1
+        n = 0
+        l = len(nums)
+        while n <= l:
+            if not d.get(n):
+                return n
+            n += 1
+        # optimal
+        '''
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
+        '''
